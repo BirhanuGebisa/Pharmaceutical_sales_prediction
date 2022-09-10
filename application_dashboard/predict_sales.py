630f85e03fc3e005205145fa91748ab4f4d54ad2
BirhanuGebisa <birhanugebisa@gmail.com>
2022-09-10 18:34:41 +0300

    dashboard: application prediction sales

---
+++ application_dashboard/predict_sales.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os, sys
+import streamlit as st
+
+sys.path.append(os.path.abspath(os.path.join('../src')))
+from log import get_logger
+from db_script import DBScript
+db = DBScript()
+
+def make_prediction(page=None):
+    train = pd.read_sql('select * from pharmaceuticalData', db.get_engine())
+    train.reset_index(drop=True)
+    train.drop('index',axis =1, inplace=True)
+    train.set_index('Date',inplace=True)
